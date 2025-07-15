4deede7c4e5370033d13d2a3aa8a15674ee658c4
Rahul Rathore <rahulrathore44@gmail.com>
2025-07-15 23:51:36 +0530

    Added the custom response body

--- library/src/test/java/org/learning/communication/TestGetAllEndPoint.java
+++ library/src/test/java/org/learning/communication/TestGetAllEndPoint.java
@@ -3,6 +3,7 @@ package org.learning.communication;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.http.Body;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
 import org.apache.http.entity.ContentType;
@@ -70,6 +71,70 @@ public class TestGetAllEndPoint {
         }
 
     }
+
+    @Test
+    @DisplayName("Verify that the server returns 200 ok status code with repose data")
+    public void test200OkStatusCode() throws Exception {
+
+        var responseBody = """
+                    {
+                      "pets": [
+                        {
+                          "id": 1,
+                          "name": "Bruno",
+                          "category": {
+                            "id": 1,
+                            "name": "Dog"
+                          },
+                          "photoUrls": [
+                            "http://localhost:8909/pics/dog.jpg"
+                          ],
+                          "tags": [
+                            {
+                              "id": 1,
+                              "name": "Four legs"
+                            }
+                          ],
+                          "status": "sold"
+                        }
+                      ]
+                    }
+                """.trim();
+
+        var body = new Body(responseBody);
+        var stub = WireMock.get("/pet/all").willReturn(WireMock.aResponse().withStatus(HttpStatus.SC_OK).withHeader(HttpHeaders.CONTENT_TYPE, configuration.getContentType().getMimeType()).withResponseBody(body));
+        server.stubFor(stub);
+        try {
+
+            var response = communication.getAll();
+
+            // Validation on response
+            Assertions.assertNotNull(response);
+
+            var responseData = response.returnResponse();
+
+            // Validation on status code
+            Assertions.assertEquals(HttpStatus.SC_OK, responseData.getStatusLine().getStatusCode());
+
+            // Validation on response header
+            var header = Arrays.stream(responseData.getHeaders(HttpHeaders.CONTENT_TYPE)).findFirst();
+
+            if (header.isPresent()) {
+                // Validation for response header
+                Assertions.assertEquals(header.get().getValue(), configuration.getContentType().getMimeType());
+            } else {
+                Assertions.fail("Response dose not have the " + HttpHeaders.CONTENT_TYPE + " header.");
+            }
+
+            // validation on response body
+            Assertions.assertNotNull(responseData.getEntity());
+
+        } finally {
+            server.removeStub(stub);
+        }
+
+    }
+
 
     @AfterAll
     public static void tearDown() {
