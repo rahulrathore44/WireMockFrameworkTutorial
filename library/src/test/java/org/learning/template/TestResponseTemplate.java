abe0d7e24091a5643df4c496af1553109f1b75ae
rahulrathore44 <rahulrathore44@gmail.com>
2025-10-11 10:47:23 +0530

    Added the test using the request model

---
+++ library/src/test/java/org/learning/template/TestResponseTemplate.java
@@ -0,0 +1,98 @@
+package org.learning.template;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.http.Body;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
+import org.apache.http.entity.ContentType;
+import org.apache.http.util.EntityUtils;
+import org.junit.jupiter.api.*;
+import org.learning.communication.Communication;
+import org.learning.communication.CommunicationImpl;
+import org.learning.config.Configuration;
+
+import java.util.Arrays;
+
+public class TestResponseTemplate {
+
+    private static WireMockConfiguration wireMockConfiguration;
+    private static WireMockServer wireMockServer;
+    private static Communication communication;
+    private static Configuration configuration;
+
+    @BeforeAll
+    public static void setUp() {
+        wireMockConfiguration = new WireMockConfiguration().templatingEnabled(true);
+        wireMockServer = new WireMockServer(wireMockConfiguration.dynamicPort());
+        wireMockServer.start();
+        configuration = new Configuration.ConfigurationBuilder().withUrl("http://localhost:" + wireMockServer.port()).withContentType(ContentType.APPLICATION_JSON).build();
+        communication = new CommunicationImpl(configuration);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if (wireMockServer.isRunning()) {
+            wireMockServer.shutdownServer();
+        }
+        wireMockConfiguration = null;
+        communication = null;
+        configuration = null;
+    }
+
+    @Test
+    @DisplayName("Verify the response template using header and body")
+    public void testResponseTemplateWithHeaderAndBody() throws Exception {
+        var requestBodyInJson = """
+                {
+                	"id": 1,
+                	"name": "Bruno",
+                	"category": {
+                		"id": 1,
+                		"name": "Dog"
+                	},
+                	"photoUrls": [
+                		"http://localhost:8909/pics/dog.jpg"
+                	],
+                	"tags": [
+                		{
+                			"id": 1,
+                			"name": "Four legs"
+                		}
+                	],
+                	"status": "sold"
+                }
+                """.stripIndent().trim();
+
+        // {{request.headers.<header-name>}}
+        // {{request.headers.Accept}}
+        var acceptHeaderValue = "{{request.headers.Accept}}";
+        // {{request.body}}
+        // {{request.method}} : To get the request HTTP method
+        // {{request.url}} : To get the request URL
+        // {{request.path.<path-param-name>}} : To ge the path parameters
+        var responseBody = "{{request.body}}";
+        var stubForPost = WireMock.post("/pet")
+                .withRequestBody(WireMock.equalToJson(requestBodyInJson, true, true))
+                .willReturn(WireMock.aResponse().withStatus(HttpStatus.SC_CREATED)
+                        .withHeader(HttpHeaders.CONTENT_TYPE, acceptHeaderValue)
+                        .withResponseBody(new Body(responseBody))
+                        .withTransformers("response-template")
+                );
+        wireMockServer.stubFor(stubForPost);
+        try {
+
+            var response = communication.create(requestBodyInJson);
+            Assertions.assertNotNull(response);
+            var responseObj = response.returnResponse();
+            var headers = responseObj.getAllHeaders();
+            var body = EntityUtils.toString(responseObj.getEntity());
+            System.out.println("Response Body: " + body);
+            System.out.println("Response Headers: " + Arrays.toString(headers));
+        } finally {
+            wireMockServer.removeStub(stubForPost);
+
+        }
+    }
+}
